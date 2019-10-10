9adabec53d01f3b55fdb0451927824b5dd2b6201
joshpied <34454009+joshpied@users.noreply.github.com>
2019-10-10 12:09:58 -0400

    Add files via upload

---
+++ Film Hunter/Film Hunter/MoviesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MoviesViewController.swift
+//  Film Hunter
+//
+//  Created by student on 2019-10-10.
+//  Copyright © 2019 Mohawk. All rights reserved.
+//  I, Joshua Piedimonte, student number 000746786, certify that this material is my original work.
+//  No other person's work has been used without due acknowledgement and I have not made my work available to anyone else.
+//
+
+import UIKit
+
+class MoviesViewController: UITableViewController {
+    
+}
